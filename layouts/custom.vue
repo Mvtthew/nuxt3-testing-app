2c7d331b69620adc27dd2a05c2d0999e21744fcb
Mateusz Ożóg <mateusz2345678@gmail.com>
2021-12-17 15:36:45 +0100

    :tada: Plugins

---
+++ layouts/custom.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h1>Pages layout</h1>
+
+        <div>
+            <slot />
+        </div>
+    </div>
+</template>
---
+++ pages/pages.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    layout: 'custom',
+};
+</script>
+
+<script setup></script>
+
+<template>
+    <div>
+        <h1>pages parent</h1>
+
+        <nuxt-link to="/">
+            Home
+        </nuxt-link>
+        <nuxt-link to="/pages/about">
+            Pages/about
+        </nuxt-link>
+
+        <NuxtChild />
+    </div>
+</template>
